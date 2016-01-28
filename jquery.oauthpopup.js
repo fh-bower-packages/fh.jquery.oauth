8b8d5bde474fa53304e26ef6404997f5ae6221b7
Ferron H <ferronrsmith@gmail.com>
2016-01-28 01:49:46 -0400

    scrollbar fix

--- jquery.oauthpopup.js
+++ jquery.oauthpopup.js
@@ -8,29 +8,29 @@
  *   http://www.gnu.org/licenses/gpl.html
  */
 (function($){
-    //  inspired by DISQUS
-    $.oauthpopup = function(options)
-    {
-        if (!options || !options.path) {
-            throw new Error("options.path must not be empty");
+  //  inspired by DISQUS
+  $.oauthpopup = function(options)
+  {
+    if (!options || !options.path) {
+      throw new Error("options.path must not be empty");
+    }
+    options = $.extend({
+      windowName: 'ConnectWithOAuth' // should not include space for IE
+      , windowOptions: 'location=0,status=0,width=800,height=400,scrollbars=1'
+      , callback: function(){
+        window.location.reload();
+        if(!window.closed) {
+          window.close();
         }
-        options = $.extend({
-            windowName: 'ConnectWithOAuth' // should not include space for IE
-          , windowOptions: 'location=0,status=0,width=800,height=400'
-          , callback: function(){ 
-        	  window.location.reload(); 
-        	  if(!window.closed) {
-            	  window.close();
-        	  }
-           }
-        }, options);
+      }
+    }, options);
 
-        var oauthWindow   = window.open(options.path, options.windowName, options.windowOptions);
-        var oauthInterval = window.setInterval(function(){
-            if (oauthWindow.closed) {
-                window.clearInterval(oauthInterval);
-                options.callback();
-            }
-        }, 1000);
-    };
+    var oauthWindow   = window.open(options.path, options.windowName, options.windowOptions);
+    var oauthInterval = window.setInterval(function(){
+      if (oauthWindow.closed) {
+        window.clearInterval(oauthInterval);
+        options.callback();
+      }
+    }, 1000);
+  };
 })(jQuery);
